38c48ef30886a127aea822b2333d24a9a8e2b3a5
voxivoid <voxivoid@hotmail.com>
2015-11-28 22:22:20 +0000

    Now a message is sent when the result of the query is empty

--- handlers/api-gene.js
+++ handlers/api-gene.js
@@ -6,23 +6,44 @@ var handlers = module.exports = [];
 
 handlers.push(validate({
 	params: {
-		geneId: joi.string().regex(/GI:[0-9]*/)
+		geneId: joi.string().regex(/gi:[0-9]+/i)
 	}
 }));
 
-handlers.push(
-	function (err, req, res, next) {
-		if(err.isBoom){
-			console.log(err.isBoom);
-			res.send("Deu erro");
+handlers.push(function (err, req, res, next) {
+		var error = {error: ""};
+		if (err.isBoom) {
+			if (err.data[0].path === "params.geneId"){
+				error.error = "Invalid geneId. It must be in the GI:<number> format.";
+				res.send(error);
+			}
+		}
+		else {
+			next(err);
 		}
 	}
-
 );
 
-handlers.push(function(req, res, next){
+handlers.push(function(req, res, next) {
 	req.app.connection.execute("select * from gene where gene_ncbi like :geneId", { geneId: req.params.geneId }, function(err, rows) {
 		if (err) return next(err);
-		res.send(rows);
+		if (rows.length < 1) {
+			console.log(rows);
+			req.isEmpty = true;
+		}
+		else {
+			req.result = rows;
+		}
+		next();		
 	});
+});
+
+handlers.push(function(req, res, next) {
+	if(req.isEmpty) {
+		var error = {error: "Gene " + req.params.geneId + " was not found."};
+		res.send(error);
+	}
+	else {
+		res.send(req.result);
+	}
 });
--- routes.js
+++ routes.js
@@ -4,7 +4,5 @@ module.exports = function (app) {
 
 	var handlers = require("require-dir")("./handlers");
 
-	console.log(handlers);
-
 	app.get("/api/gene/:geneId", handlers["api-gene"]);
 }
