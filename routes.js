647abe8af829fde9460840a3a4d81a1a720b1277
voxivoid <voxivoid@hotmail.com>
2015-12-09 11:48:55 +0000

    Updated routes

--- routes.js
+++ routes.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
 
 	var handlers = require("require-dir")("./handlers");
 
-	app.get("/api/gene/", handlers["api-gene"]);
-	app.get("/api/protein/", handlers["api-protein"]);
-	app.get("/api/pathway/", handlers["api-pathway"]);
+	app.get("/~biosurfdb/api/gene/", handlers["api-gene"]);
+	app.get("/~biosurfdb/api/protein/", handlers["api-protein"]);
+	app.get("/~biosurfdb/api/pathway/", handlers["api-pathway"]);
 
 }
